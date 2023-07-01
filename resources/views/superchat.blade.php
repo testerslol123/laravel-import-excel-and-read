31a73c7b6ea69af7c00126af1b33eaf3fa2af487
DanzKraken <testerslol123@gmail.com>
2023-07-01 23:40:17 +0700

    Added TExt update

--- resources/views/superchat.blade.php
+++ resources/views/superchat.blade.php
@@ -14,14 +14,32 @@
       <a href="/display-data">Ke Tunjukkan Data</a>
     </div>
     <h2>Model</h2>
-    <h3>
+    <p>Model berikut digunakan untuk memprediksi total penghasilan superchat member hololive production</p>
+    <p>Cara menggunakan :</p>
+    <ul class="list-group">
+      <li class="list-group-item">masukkan angka total view pada kolom total view</li>
+      <li class="list-group-item">masukkan angka ccv pada kolom ccv</li>
+      <li class="list-group-item">prediksi total superchat akan muncul</li>
+    </ul>
+    <h3 class="mt-3 mb-3">
       Superchat = 3.062 x10<sup>8</sup> + totalview x 5.750 x 10<sup>1</sup> + ccv x 5.246 x 10<sup>5</sup>
     </h3>
     <div>
-      <p>Superchat = 3.062 x10<sup>8</sup> + <input type='number' name='totalview' id="totalview" onChange="hitungHasilSuperchat()" /> x 5.750 x 10<sup>1</sup> + <input type='number' name='ccv' id="ccv" onChange="hitungHasilSuperchat()" /> x 5.246 x 10<sup>5</sup></p>
+      <p>Superchat = 3.062 x10<sup>8</sup> + 
+        <input type='number' name='totalview' id="totalview" onChange="hitungHasilSuperchat()" placeholder="masukkan total view" /> x 5.750 x 10<sup>1</sup> + 
+        <input type='number' name='ccv' id="ccv" onChange="hitungHasilSuperchat()" placeholder="masukkan ccv" /> x 5.246 x 10<sup>5</sup>
+      </p>
+      <p class="text-danger">Mohon isi total view dan ccv nya terlebih dahulu</p>
     </div>
     <div>
       <p>Superchat = <input disabled type='number' name="hasil" id="hasilperkalian" /></p>
+      <p>Hasil prediksi total superchat dari perhitungan model diatas adalah Rp (hasil superchat)</p>
+      <p>Keterangan:</p>
+      <ul class="list-group">
+        <li class="list-group-item">Superchat: hasil prediksi pendapatan superchat dalam satuan rupiah</li>
+        <li class="list-group-item">Total View: total orang yang menonton vidio live Vtuber dalam satuan angka</li>
+        <li class="list-group-item">CCV: Rata-rata penonton bersamaan yang menonton siaran langsung (live streaming) dari saluran ini.</li>
+      </ul>
     </div>
 
 
